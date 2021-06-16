8bc29bae25eb5cc37850d2718fd2675302b87257
ordegani <oradeganii@gmail.com>
2021-06-16 14:16:23 +0300

    add deleteAll

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const deleteCompletedBtn = document.querySelector(".deleteCompletedBtn");
 const input_field_title = document.querySelector(".input_field_title");
 const updateTask = document.querySelector("input[name='update-task']");
 const saveNewTask = document.querySelector("[data-update]");
+const container = document.createElement("div");
+
+//DATA>
+const data = [];
 
 function createTask() {
   //add if loop to check if a task has already been added to list, and if it has been added don't add it again.
   const taskName = updateTask.value;
   const newTask = document.createElement("div");
+  data.push({ task: updateTask.value });
   const deleteTask = document.createElement("button");
   deleteTask.textContent = "Delete";
   const editTask = document.createElement("button");
@@ -18,6 +23,13 @@ function createTask() {
   const completeTask = document.createElement("button");
   completeTask.textContent = "Complete";
   newTask.append(taskName, deleteTask, editTask, completeTask);
-  body.appendChild(newTask);
+  container.appendChild(newTask);
+  body.appendChild(container);
 }
 saveNewTask.addEventListener("click", createTask);
+
+function deleteAll() {
+  const data = [];
+  container.remove();
+}
+deleteAllBtn.addEventListener("click", deleteAll);
